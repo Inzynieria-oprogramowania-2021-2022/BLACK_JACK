4954bb7b06f7c11fdc3c6dd7fb36f7446d111dda
Kszanu <66957175+RafalChrzanowski@users.noreply.github.com>
2022-05-02 21:23:54 +0200

    Add files via upload
    To jest kod on jest w trakcie prac

---
+++ BlackJack/BlackJack/BlackJack.cpp
@@ -0,0 +1,73 @@
+﻿
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h> 
+using namespace std;
+/*ToDo:
+1.Pasuje by była jaka pentla żeby kończyło program dopiero po kliknięciu 4
+2.Zrobić w miarę sensowną instrukcję
+*/
+int LosowanieKarty() {
+    srand(time(NULL));
+    int liczba = (rand() % 11) + 1;
+    cout << liczba;
+    return liczba;
+}
+int punkty;
+void Menu(int x) {
+    switch (x)
+    {
+    case 1://funkcja gry
+        cout << "Rozgrywka";
+        break;
+    case 2://funkcja sklepu
+        cout << "Sklep";
+        break;
+    case 3://support
+        cout << "Suport";
+        break;
+    case 4://instrukca
+        cout << "instrukcja";
+        cout << "Walet, Dama, Król – 10 punktów As – 1 lub 11 punktów, w zależności od tego, która opcja jest korzystniejsza dla gracza";
+        cout << "Więc dla ułatwienia te karty będą pokazywać się w ten sposób";
+        //ToDo.2
+        break;
+    case 5://exit
+        cout << "Pa Pa";
+        //exit;
+        break;
+    default:
+        cout << "Jesli to widzisz to wystapił jakis problem, proszę o kontakt z supportem";
+        break;
+    }
+}
+
+void Start() {
+    int x;
+    cout << "Witam w BlackJaku!\n ";//jakiś opis czy coś
+    cout << "Twoje Menu:\n";
+    cout << "1.Rozpocznij rozgrywke.\n";
+    cout << "2.Sklep.\n";
+    cout << "3.Support.\n";
+    cout << "4.zasady/instrukcja.\n";
+    cout << "5.Exit.\n";
+    cout << "Wybierz swoja akcje: ";
+    cin >> x;
+    Menu(x);
+}
+
+int main()
+{
+    Start();
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
